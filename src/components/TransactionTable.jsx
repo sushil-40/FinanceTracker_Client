a3ee18067d8ca0a59466b505bd04fa7d31994578
Sushil <grayson.9999@gmail.com>
2025-03-28 09:51:36 +1000

    adding input field in transaction table for search functionality and adding add transaction button

--- src/components/TransactionTable.jsx
+++ src/components/TransactionTable.jsx
@@ -1,6 +1,7 @@
 import Table from "react-bootstrap/Table";
 import { useUser } from "../context/UserContext";
-
+import { Button, Form } from "react-bootstrap";
+import { FaPlusSquare } from "react-icons/fa";
 export const TransactionTable = () => {
   const { transactions } = useUser();
   console.log(transactions);
@@ -11,39 +12,51 @@ export const TransactionTable = () => {
   }, 0);
 
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Date</th>
-          <th>Title</th>
-          <th>Out</th>
-          <th>In</th>
-        </tr>
-      </thead>
-      <tbody>
-        {transactions.length > 0 &&
-          transactions.map((t, i) => (
-            <tr key={t._id}>
-              <td>{i + 1}</td>
-              <td>{t.createdAt.slice(0, 10)}</td>
-              <td>{t.title}</td>
-              {t.type === "expenses" && (
-                <>
-                  <td>-${t.amount}</td>
-                  <td></td>
-                </>
-              )}
-              {t.type === "income" && (
-                <>
-                  <td></td>
-                  <td>${t.amount}</td>
-                </>
-              )}
-            </tr>
-          ))}
+    <>
+      <div className="d-flex justify-content-between pt-3 mb-4">
+        <div>{transactions.length} transaction(s) found.</div>
+        <div>
+          <Form.Control type="text" />
+        </div>
+        <div>
+          <Button>
+            <FaPlusSquare /> Add New Transaction
+          </Button>
+        </div>
+      </div>
+      <Table striped hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Date</th>
+            <th>Title</th>
+            <th>Out</th>
+            <th>In</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.length > 0 &&
+            transactions.map((t, i) => (
+              <tr key={t._id}>
+                <td>{i + 1}</td>
+                <td>{t.createdAt.slice(0, 10)}</td>
+                <td>{t.title}</td>
+                {t.type === "expenses" && (
+                  <>
+                    <td className="out">-${t.amount}</td>
+                    <td></td>
+                  </>
+                )}
+                {t.type === "income" && (
+                  <>
+                    <td></td>
+                    <td className="in">${t.amount}</td>
+                  </>
+                )}
+              </tr>
+            ))}
 
-        {/* <tr>
+          {/* <tr>
           <td>2</td>
           <td>23-03-2025</td>
 
@@ -60,12 +73,15 @@ export const TransactionTable = () => {
           <td>$500</td>
         </tr> */}
 
-        <tr className="fw-bold text-center">
-          <td colSpan={3}>Total Balance</td>
+          <tr className="fw-bold text-center">
+            <td colSpan={3}>Total Balance</td>
 
-          <td colSpan={2}>{balance}</td>
-        </tr>
-      </tbody>
-    </Table>
+            <td colSpan={2} className={`fs-5 ${balance > 0 ? "in" : "out"}`}>
+              {balance}
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
   );
 };
